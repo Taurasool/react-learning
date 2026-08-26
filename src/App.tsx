3a732c97003386082102e8f80137a255c905a560
Tauseef Rasool <rasooltauseef43@gmail.com>
2026-08-26 03:49:28 -0700

    Add StudentCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Header from "./Components/Component.tsx";
 import Footer from "./Components/Footer.tsx";
-
+import StudentCard from "./Components/StudentCard.tsx";
 function App(){
     return (
         <>
              <Header />
              <Footer />
+             <StudentCard />
+             <StudentCard />
+             <StudentCard />
         </>
     );
 }
